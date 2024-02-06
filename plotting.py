8ba4939149f539fcfe55da2f8d939f32384e5a53
Matthieu Bulté <matthieu.bulte.06@gmail.com>
2024-02-06 17:22:01 +0100

    Progress

--- plotting.py
+++ plotting.py
@@ -6,6 +6,23 @@ def compute_critical_val(df, quantiles_field, alpha):
     def _comp(qs): return np.interp(alpha, computed_alphas, qs)
     return df[quantiles_field].apply(_comp)
 
+def plot_sqrt_T_mu(df):
+    for phi in np.sort(df.phi.unique()):
+        if phi == 1:
+            continue
+        sub_df = df[df['phi'] == phi].copy()
+        grped = sub_df.groupby('sample_size').mean(numeric_only=True).reset_index()
+        label = r'$0\ (H_0)$' if phi == 0 else f'{phi:.1f}'
+        plt.plot(grped['sample_size'], np.sqrt(grped['sample_size']*grped['err']), label=label, marker='o', markersize=4)
+
+    plt.grid(True)
+    plt.xlabel('Sample Size')
+    plt.xlim(left=0)
+    plt.ylabel(r'$\sqrt{T}d(\mu, \hat\mu_T)$')
+    ax = plt.gca()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
 def plot_test_for(df, statname, onesided=False):
     alpha = 0.05
 
@@ -22,19 +39,25 @@ def plot_test_for(df, statname, onesided=False):
     for phi in results.phi.unique():
         sub_df = results[results['phi'] == phi]
 
-        plt.scatter(sub_df['sample_size'], sub_df.rejection_rate, s=5)
-        if phi == 0:
-            plt.plot(sub_df['sample_size'], sub_df.rejection_rate, label=r'$0\ (H_0)$')
-        else:
-            plt.plot(sub_df['sample_size'], sub_df.rejection_rate, label=np.round(phi,1))
+        label = r'$0\ (H_0)$' if phi == 0 else np.round(phi,1)
+        plt.plot(sub_df['sample_size'], sub_df.rejection_rate, label=label, marker='o', markersize=4)#, linewidth=0.75)
+        # plt.scatter(, s=5)
+        # if phi == 0:
+        #     plt.plot(sub_df['sample_size'], sub_df.rejection_rate, label=)
+        # else:
+        #     plt.plot(sub_df['sample_size'], sub_df.rejection_rate, label=label)
     plt.xlabel('Sample Size')
-    plt.ylabel('Rejection Rate')
-    plt.legend(title=r'$\varphi$')
-    plt.grid('on')
-    plt.yticks([0.05,0.1,0.2,0.3,0.4,0.5,0.6,.7,.8,.9,1]);
+    plt.ylabel('Rejection Rate (%)')
+    # plt.legend(title=r'$\varphi$', ncols=results.phi.nunique())
+    plt.grid(True)
+    plt.xlim(left=0)
+    plt.yticks([0.05,0.1,0.2,0.3,0.4,0.5,0.6,.7,.8,.9,1], [5,10,20,30,40,50,60,70,80,90,100]);
+    ax = plt.gca()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
 
 def plot_alpha_alpha(df, statname, samplesize, onesided=False):
-    target_sizes = np.linspace(0, .1, 40)
+    target_sizes = np.linspace(0, .1, 5)
     for phi in np.sort(df.phi.unique()):
         sub_df = df[(df['sample_size'] == samplesize) & (df['phi'] == phi)]
 
@@ -52,10 +75,17 @@ def plot_alpha_alpha(df, statname, samplesize, onesided=False):
             
         label = r'$0\ (H_0)$' if phi == 0 else f'{phi:.1f}'
         # plt.scatter(target_sizes, emp_sizes, label=label, s=5)
-        plt.plot(target_sizes, emp_sizes, label=label, marker='o', markersize=3)
+        plt.plot(target_sizes, emp_sizes, label=label, marker='o', markersize=4)#, linewidth=0.75)
 
-    plt.legend(title=r'$\varphi$')
-    plt.xlabel('Test level')
-    plt.ylabel('Rejection Rate')
-    plt.ylim(0,1)
-    plt.grid('on')
+    # plt.legend(title=r'$\varphi$')
+    plt.plot(target_sizes, target_sizes, color='black', linestyle='--')#, linewidth=0.75)
+    plt.xlabel('Test level (%)')
+    plt.ylabel('Rejection Rate (%)')
+    plt.yticks([0.1,0.2,0.3,0.4,0.5,0.6,.7,.8,.9,1], [10,20,30,40,50,60,70,80,90,100]);
+    plt.xticks([0, 0.02, 0.04, 0.06, 0.08 , 0.1], [0, 2, 4, 6, 8, 10]);
+    plt.xlim(0, target_sizes[-1])
+    plt.ylim(0,1.05)
+    plt.grid(True)
+    ax = plt.gca()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
--- scripts/call_single_sim.sh
+++ scripts/call_single_sim.sh
@@ -12,6 +12,4 @@ cd $src_path
 module load tools
 module load anaconda3/2023.03
 
-pip3 install --user --quiet -r ../requirements.txt
-
 PYTHONPATH="." python3 $main_path --repeat-id=$2 --path=$3 --config-id=$PBS_ARRAYID
